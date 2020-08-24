201fe0435a059cd7cb57fe954b0c414abe104cb2
adriansokolowski <37288976+adriansokolowski@users.noreply.github.com>
2020-08-25 01:25:16 +0200

    Add movies seeder and faker

---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,58 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Movie;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+use function PHPSTORM_META\map;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Movie::class, function (Faker $faker) {
+    return [
+        'user_id' => factory(\App\User::class)->create()->id,
+        'title' => $faker->name,
+        'release_date' => $faker->dateTime,
+        'description' => $faker->text,
+        'trailer' => $faker->text(10),
+        'fdb' => $faker->randomNumber,
+        'up' => $faker->randomNumber,
+        'down' => $faker->randomNumber,
+        'views' => $faker->randomNumber,
+        'slider' => $faker->randomNumber,
+        'report' => 0,
+        'html' => $faker->text,
+        'last_view' => now(),
+        'imdb_rate' => 7,
+        'visible' => 1
+    ];
+});
+
+// $table->id();
+//             $table->foreignId('user_id')->constrained();
+//             $table->string('title');
+//             $table->dateTime('release_date');
+//             $table->text('description');
+//             $table->string('trailer', 15)->nullable();
+//             $table->unsignedInteger('fdb');
+//             $table->unsignedInteger('up')->default(0);
+//             $table->unsignedInteger('down')->default(0);
+//             $table->unsignedInteger('views')->default(0);
+//             $table->unsignedInteger('slider')->nullable();
+//             $table->string('report')->default(0);
+//             $table->text('html')->default('');
+//             $table->dateTime('last_view');
+//             $table->float('imdb_rate');
+//             $table->smallInteger('visible')->default(1);
+//             $table->timestamps();
